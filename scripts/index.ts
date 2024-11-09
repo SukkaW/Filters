d036ed7e6306949407b39c9c591612219be19162
SukkaW <isukkaw@gmail.com>
2024-11-10 01:04:45 +0800

    chore: add 404.html

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,7 +1,24 @@
+import path from 'node:path';
 import { buildFilter } from './filters';
 import { buildScriptlets } from './scriptlets';
+import fs from 'node:fs';
+import { PUBLIC_DIR } from './_constants';
 
 (async () => {
   await buildFilter();
   await buildScriptlets();
+
+  fs.writeFileSync(
+    path.join(PUBLIC_DIR, '404.html'),
+    [
+      '# <pre>',
+      '#########################################',
+      '# Sukka\'s Filters - 404 Not Found',
+      '# Homepage: https://github.com/SukkaW/Filters',
+      '# License: MIT',
+      '################## EOF ##################',
+      '</pre>',
+      ''
+    ].join('\n')
+  );
 })();
