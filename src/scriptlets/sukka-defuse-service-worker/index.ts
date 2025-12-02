a4e99c5720fb6bd4290a9dee0bd5601121f89916
SukkaW <isukkaw@gmail.com>
2025-12-03 04:14:27 +0800

    fix(scriptlet): sw defuse

--- src/scriptlets/sukka-defuse-service-worker/index.ts
+++ src/scriptlets/sukka-defuse-service-worker/index.ts
@@ -2,34 +2,41 @@ import { $console } from '../_utils';
 
 /// sukka-defuse-service-worker.js
 (function sukkaDefuseServiceWorker() {
-  Object.defineProperty(navigator.serviceWorker, 'register', {
-    value: (async (...args) => {
-      $console.log('[sukka-defuse-service-worker] blocked service worker registration', args);
+  try {
+    Object.defineProperty(navigator.serviceWorker, 'register', {
+      value: (async (...args) => {
+        $console.log('[sukka-defuse-service-worker] blocked service worker registration', args);
 
-      try {
-        const regs = await navigator.serviceWorker.getRegistrations();
-        if (regs.length > 0) {
-          $console.log('[sukka-defuse-service-worker] unregistering existing registrations:', regs);
+        try {
+          const regs = await navigator.serviceWorker.getRegistrations();
+          if (regs.length > 0) {
+            $console.log('[sukka-defuse-service-worker] unregistering existing registrations:', regs);
 
-          for (const reg of regs) {
-            const url = reg.active?.scriptURL ?? reg.installing?.scriptURL ?? reg.waiting?.scriptURL ?? 'unknown';
-            // eslint-disable-next-line no-await-in-loop -- let's do this one by one
-            if (await reg.unregister()) {
-              $console.log('[sukka-defuse-service-worker] successfully unregister:', url, reg.scope, reg);
-            } else {
-              $console.log('[sukka-defuse-service-worker] unfinished unregister:', url, reg.scope, reg);
+            for (const reg of regs) {
+              const url = reg.active?.scriptURL ?? reg.installing?.scriptURL ?? reg.waiting?.scriptURL ?? 'unknown';
+              // eslint-disable-next-line no-await-in-loop -- let's do this one by one
+              if (await reg.unregister()) {
+                $console.log('[sukka-defuse-service-worker] successfully unregister:', url, reg.scope, reg);
+              } else {
+                $console.log('[sukka-defuse-service-worker] unfinished unregister:', url, reg.scope, reg);
+              }
             }
+            await Promise.all(regs.map(reg => reg.unregister()));
+
+            $console.log('[sukka-defuse-service-worker] unregistered all existing registrations!');
           }
-          await Promise.all(regs.map(reg => reg.unregister()));
-
-          $console.log('[sukka-defuse-service-worker] unregistered all existing registrations!');
+        } catch (e) {
+          $console.error('[sukka-defuse-service-worker] error while unregistering existing registrations:', e);
         }
-      } catch (e) {
-        $console.error('[sukka-defuse-service-worker] error while unregistering existing registrations:', e);
-      }
 
-      // eslint-disable-next-line sukka/unicorn/error-message -- intentionally empty message
-      throw new Error('');
-    }) satisfies typeof navigator.serviceWorker.register // ensure the new method has the same type as the original
-  });
+        // eslint-disable-next-line sukka/unicorn/error-message -- intentionally empty message
+        throw new Error('');
+      }) satisfies typeof navigator.serviceWorker.register // ensure the new method has the same type as the original
+    });
+  } catch (e) {
+    if (typeof e === 'object' && e !== null && 'name' in e && e.name === 'SecurityError') {
+      return;
+    }
+    $console.error('[sukka-defuse-service-worker] failed to defuse service worker:', e);
+  }
 }());
