705ad9d9ad10f234ef8cfe91307ed678d73d7f2f
SukkaW <isukkaw@gmail.com>
2026-02-07 17:09:53 +0800

    perf(scriptlet): simpify unregister

--- src/scriptlets/sukka-defuse-service-worker/index.ts
+++ src/scriptlets/sukka-defuse-service-worker/index.ts
@@ -5,23 +5,20 @@ import { $console } from '../_utils';
   try {
     Object.defineProperty(navigator.serviceWorker, 'register', {
       value: (async (...args) => {
-        $console.log('[sukka-defuse-service-worker] blocked service worker registration', args);
-
         try {
           const regs = await navigator.serviceWorker.getRegistrations();
           if (regs.length > 0) {
             $console.log('[sukka-defuse-service-worker] unregistering existing registrations:', regs);
 
-            for (const reg of regs) {
+            await Promise.all(regs.map(async reg => {
               const url = reg.active?.scriptURL ?? reg.installing?.scriptURL ?? reg.waiting?.scriptURL ?? 'unknown';
-              // eslint-disable-next-line no-await-in-loop -- let's do this one by one
-              if (await reg.unregister()) {
+              const result = await reg.unregister();
+              if (result) {
                 $console.log('[sukka-defuse-service-worker] successfully unregister:', url, reg.scope, reg);
               } else {
-                $console.log('[sukka-defuse-service-worker] unfinished unregister:', url, reg.scope, reg);
+                $console.warn('[sukka-defuse-service-worker] unfinished unregister:', url, reg.scope, reg);
               }
-            }
-            await Promise.all(regs.map(reg => reg.unregister()));
+            }));
 
             $console.log('[sukka-defuse-service-worker] unregistered all existing registrations!');
           }
@@ -29,6 +26,7 @@ import { $console } from '../_utils';
           $console.error('[sukka-defuse-service-worker] error while unregistering existing registrations:', e);
         }
 
+        $console.log('[sukka-defuse-service-worker] blocked service worker registration', args);
         // eslint-disable-next-line sukka/unicorn/error-message -- intentionally empty message
         throw new Error('');
       }) satisfies typeof navigator.serviceWorker.register // ensure the new method has the same type as the original
