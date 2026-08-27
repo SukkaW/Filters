6b2929271a3c0f7d44685e3a2795568e1dcf4223
SukkaW <isukkaw@gmail.com>
2026-08-27 10:27:00 +0800

    feat(filters): update

--- src/url-redirector/index.ts
+++ src/url-redirector/index.ts
@@ -817,7 +817,6 @@ export default [
       'node.windy.com',
       'imgproxy.windy.com',
       'storage.googleapis.com/intervals-icu-images/',
-      'upload.wikimedia.org',
 
       'd3g0gp89917ko0.cloudfront.net' // wikidot
     ] as const).map(host => ({
